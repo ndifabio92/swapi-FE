6b0e9451ea5773396570764f146d7e2e465f08c6
Nicolas Di Fabio <nicolasdifabio92@gmail.com>
2023-06-10 18:58:50 -0300

    se elimina algo que no se estaba usando

--- src/components/ui/Navbar/Navbar.jsx
+++ src/components/ui/Navbar/Navbar.jsx
@@ -1,9 +1,7 @@
-import { Link, NavLink, useNavigate } from 'react-router-dom';
+import { Link, NavLink } from 'react-router-dom';
 
 
 const Navbar = () => {
-
-    const navigate = useNavigate();
 
     return (
         <nav className="navbar navbar-expand-sm navbar-dark bg-dark p-2">
