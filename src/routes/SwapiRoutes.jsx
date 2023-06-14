549f57986830363c78418e405e44127fe3f40fe8
Nicolas Di Fabio <nicolasdifabio92@gmail.com>
2023-06-14 17:49:11 -0300

    se deja como estaba el router

--- src/routes/SwapiRoutes.jsx
+++ src/routes/SwapiRoutes.jsx
@@ -1,15 +1,15 @@
-import React, { lazy } from "react";
+import React from "react";
 import { Route, Routes } from "react-router-dom";
 import Navbar from "../components/ui/Navbar/Navbar";
+
 import NotFound from "../pages/not-found/NotFound";
+import Films from "../pages/movies/Films";
+import People from "../pages/people/People";
+import Planets from "../pages/planets/Planets";
+import Starships from "../pages/starships/Starships";
+import Vehicles from "../pages/vehicles/Vehicles";
+import PeopleDetail from "../pages/people/PeopleDetail";
 
-
-const Films = lazy(() => import('../pages/movies/Films'));
-const People = lazy(() => import('../pages/people/People'));
-const PeopleDetail = lazy(() => import('../pages/people/PeopleDetail'));
-const Planets = lazy(() => import('../pages/planets/Planets'));
-const Starships = lazy(() => import('../pages/starships/Starships'));
-const Vehicles = lazy(() => import('../pages/vehicles/Vehicles'));
 
 export const SwapiRoutes = () => {
     return (
