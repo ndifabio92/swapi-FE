9085f624811659120f6e9317bef806f298087027
Nicolas Di Fabio <nicolasdifabio92@gmail.com>
2023-06-17 15:19:32 -0300

    se intenta solucionar porque ya no funcionan los botones para ver mas

--- src/routes/SwapiRoutes.jsx
+++ src/routes/SwapiRoutes.jsx
@@ -19,14 +19,14 @@ export const SwapiRoutes = () => {
             <Navbar />
             <div style={{ marginLeft: "32px", marginRight: "32px" }}>
                 <Routes>
-                    <Route path="/" element={<HomePage/>} />
-                    <Route path="/films" element={<Films />} />
-                    <Route path="/films/:id" element={<FilmDetail />} />
-                    <Route path="/characters" element={<Characters />} />
-                    <Route path="/characters/:id" element={<CharacterDetail />}/>
-                    <Route path="/planets" element={<Planets />} />
-                    <Route path="/starships" element={<Starships />} />
-                    <Route path="/vehicles" element={<Vehicles />} />
+                    <Route exact path="/" element={<HomePage/>} />
+                    <Route exact path="/films" element={<Films />} />
+                    <Route exact path="/films/:id" element={<FilmDetail />} />
+                    <Route exact path="/characters" element={<Characters />} />
+                    <Route exact path="/characters/:id" element={<CharacterDetail />}/>
+                    <Route exact path="/planets" element={<Planets />} />
+                    <Route exact path="/starships" element={<Starships />} />
+                    <Route exact path="/vehicles" element={<Vehicles />} />
 
                     <Route path="*" element={<NotFound />} />
                 </Routes>
